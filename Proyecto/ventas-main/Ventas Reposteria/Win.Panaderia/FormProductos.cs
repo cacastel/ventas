6b1cd334f0cfb1a7dc0ef4694f2f109a318cc3b5
cacastel <78243953+cacastel@users.noreply.github.com>
2021-03-08 12:19:16 -0600

    Update

---
+++ Proyecto/ventas-main/Ventas Reposteria/Win.Panaderia/FormProductos.cs
@@ -0,0 +1,310 @@
+﻿using BL.Rentas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Panaderia
+{
+    public partial class FormProductos : Form
+    {
+        ProductosBL _productos;
+        CategoriasBL _categorias;
+        TiposBL _tiposBL;
+
+        public object listaCategoriasBindingSource { get;  set; }
+
+        public FormProductos()
+        {
+            InitializeComponent();
+
+            _productos = new ProductosBL();
+            listaProductosBindingSource.DataSource = _productos.ObtenerProductos();
+
+              _categorias = new CategoriasBL();
+           
+            listaCategoriasBindingSource.DataSource = _categorias.ObtenerCategorias();
+                
+            _tiposBL = new TiposBL();
+            listaProductosBindingSource.DataSource = _tiposBL.ObtenerTipos();
+
+            
+
+        }
+
+
+
+
+
+        private void productosBLDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FormProductos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void productosBLBindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listaProductosDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void activoCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void descripcionLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void descripcionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void existenciaLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void existenciaTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void idLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void idTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void precioLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void precioTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void activoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMoveFirstItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMovePreviousItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorSeparator_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorPositionItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorCountItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorSeparator1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMoveNextItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMoveLastItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorSeparator2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listaProductosBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listaProductosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaProductosBindingSource.EndEdit();
+            var producto = (Producto)listaProductosBindingSource.Current;
+
+            if (fotoPictureBox.Image != null) {
+
+                producto.Foto = Program.imageToByteArray(fotoPictureBox.Image);
+
+            }
+
+            else
+            {
+                producto.Foto = null;
+
+            }
+
+
+            var resultado = _productos.GuardarProducto(producto);
+
+            if (resultado.Exitoso == true)
+            {
+                listaProductosBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+                MessageBox.Show("Producto Guardado");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+
+        private void bindingNavigatorAddNewItem_Click_1(object sender, EventArgs e)
+        {
+            _productos.AgregarProducto();
+            listaProductosBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorDeleteItem_Click_1(object sender, EventArgs e)
+        {
+            if (idTextBox.Text != "")
+            {
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                { 
+                    var id = Convert.ToInt32(idTextBox.Text);
+                    Eliminar(id);
+               }
+            }     
+        }
+
+        private void Eliminar(int id)
+        {
+            var resultado = _productos.EliminarProducto(id);
+
+            if (resultado == true)
+            {
+                listaProductosBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar un producto");
+            }
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+            DeshabilitarHabilitarBotones(true);
+            Eliminar(0);
+        }
+
+        private void fotoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fotoPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var producto = (Producto)listaProductosBindingSource.Current;
+            if(producto!=null)
+            {
+                openFileDialog1.ShowDialog();
+                var archivo = openFileDialog1.FileName;
+
+                if (archivo != "")
+                {
+                    var fileInfo = new FileInfo(archivo);
+                    var fileStream = fileInfo.OpenRead();
+                    fotoPictureBox.Image = Image.FromStream(fileStream);
+
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Cree un producto antes de asignarle una imagen");
+            }
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fotoPictureBox.Image = null;
+        }
+    }
+}
---
+++ Proyecto/ventas-main/Ventas Reposteria/Win.Panaderia/FormRproducto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Panaderia
+{
+    public partial class FormRproducto : Form
+    {
+        public FormRproducto()
+        {
+            InitializeComponent();
+        }
+
+        private void FormRproducto_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
